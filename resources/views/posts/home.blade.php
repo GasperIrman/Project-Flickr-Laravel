107c67c86b3c7c00493bf095c0f148daa02fbada
Gašper <gasper.irman@gmail.com>
2018-11-06 11:29:31 +0100

    Added posts

--- resources/views/posts/home.blade.php
+++ resources/views/posts/home.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
 	<h1>Home</h1>
-	<p>This is the home page</p>
+	@if(count($posts) > 0)
+		@foreach($posts as $post)
+			<div class="well">
+				<h3>{{$post->title}}</h3>
+				<p>{{$post->description}}</p>
+				<small>{{$post->created_at}}</small>
+			</div>
+		@endforeach
+	@else
+		<p>No posts found</p>
+	@endif
 @endsection
